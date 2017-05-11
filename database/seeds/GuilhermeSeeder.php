98fc283378d77b930ade87477de076e370cde2c2
GuilhermeSDias <guilhermedude.7@gmail.com>
2017-05-11 13:03:57 -0300

    Guilherme - Teste Comitado (Funcionou no Meu !!!)

--- database/seeds/GuilhermeSeeder.php
+++ database/seeds/GuilhermeSeeder.php
@@ -11,12 +11,6 @@ class GuilhermeSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Entities\User::class, 100)->create();
-        factory(App\Entities\TypeUser::class, 5)->create();
-        factory(App\Entities\UserTypeUser::class, 100)->create();
-        factory(App\Entities\Event::class, 100)->create();
-        factory(App\Entities\TypeActivity::class, 100)->create();
-        factory(App\Entities\Activity::class, 10)->create();
         factory(App\Entities\TypeActivityUser::class, 5)->create();
         factory(App\Entities\ActivityUser::class, 5)->create();
     }
